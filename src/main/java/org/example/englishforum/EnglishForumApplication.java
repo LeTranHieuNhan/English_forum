81380b0cd705f296fc887147baad1c111f59c0bf
d2fLucifer <d2f.working@gmail.com>
2024-02-29 12:41:58 +0700

    first commit

---
+++ src/main/java/org/example/englishforum/EnglishForumApplication.java
@@ -0,0 +1,13 @@
+package org.example.englishforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishForumApplication.class, args);
+    }
+
+}
